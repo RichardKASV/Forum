4038634839351e8ae257f9afd7f14cf4dbefc084
Samuel Rak <samuel.rak@kosickaakademia.sk>
2022-11-03 12:45:25 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");//ahoj // Ahoj, tu Denis
+        if(a%3 == 0)
+            m=6;
+        else
+            m=-4
     }
 
 }
