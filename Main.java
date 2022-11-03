85acdadd5dafc44e8f6db9084e017cddd1ec5b05
Viktor Kriššák <viktor.krissak@kosickaakademia.sk>
2022-11-03 13:53:50 +0100

    Added Point

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");//ahoj // Ahoj, tu Denis
+        System.out.println("Hello world!");//ahoj // Ahoj, tu Denis //sevas tu Viktor
+    }
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 
 }
