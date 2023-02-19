65124b1d66b0152d511f0c7a64aa949f63db6314
Viktor Kriššák <viktor.krissak@kosickaakademia.sk>
2023-02-19 14:59:50 +0100

    Trying add login try nb:3

---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service'
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AngularFireAuth } from "@angular/fire/compat/auth";
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  signupForm: FormGroup;
+  firebaseErrorMessage: string;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private afAuth: AngularFireAuth,
+    private formBuilder: FormBuilder
+  ) {
+    this.firebaseErrorMessage = '';
+    this.signupForm = this.formBuilder.group({
+      displayName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  signup() {
+    if (this.signupForm.invalid) {
+      return;
+    }
+
+    this.authService.signupUser(this.signupForm.value).then((result) => {
+      if (result == null) {
+        this.router.navigate(['/dashboard']);
+      } else if (result.isValid == false) {
+        this.firebaseErrorMessage = result.message;
+      }
+    }).catch(() => {});
+  }
+}
